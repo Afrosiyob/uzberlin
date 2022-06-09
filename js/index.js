56811bca38517b74ccf843ae55f1d019976c657e
Afrosiyob <45900728+Afrosiyob@users.noreply.github.com>
2022-06-09 16:52:31 +0500

    add
    add

---
+++ js/index.js
@@ -0,0 +1,15 @@
+$( document ).ready( function () {
+
+    // $( window ).scroll( function () {
+    //     console.log( $( this ).scrollTop() );
+    //     if ( $( this ).scrollTop() < 300 ) {
+    //         $( "#headerNav" ).css( {
+    //             height: "65px"
+    //         } );
+    //     } else {
+    //         $( "#headerNav" ).css( {
+    //             height: "55px"
+    //         } );
+    //     }
+    // } );
+} );
